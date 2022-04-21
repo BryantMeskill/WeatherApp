4abc381d7f52463b3adb48d5ecaccf866cb7717d
Bryant Meskill <bryant.meskill@gmail.com>
2022-04-21 16:11:09 -0500

    State added for image control, more weather options added.

--- src/components/main.js
+++ src/components/main.js
@@ -1,28 +1,39 @@
-import React, { useEffect } from "react";
-const Main = ({ city, desc, temp, feelsLike, humidity }) => {
+import React, { useState, useEffect } from "react";
+const Main = ({ isLoading, city, desc, temp, feelsLike, humidity }) => {
+  const [imgChange, setImgChange] = useState(
+    "http://openweathermap.org/img/wn/02d@2x.png"
+  );
+
   useEffect(() => {
-    const img = document.querySelector("img");
     if (desc.includes("CLEAR")) {
       document.body.style.backgroundColor = "#48b4e0";
-      document.body.style.transition = "1s";
-      img.src = "http://openweathermap.org/img/wn/01d@2x.png";
+      setImgChange("http://openweathermap.org/img/wn/01d@2x.png");
     } else if (desc.includes("CLOUD")) {
-      document.body.style.backgroundColor = "#818681";
-      document.body.style.transition = "1s";
-      img.src = "http://openweathermap.org/img/wn/03d@2x.png";
-      img.style.transition = "1s";
-    } else {
-      img.src = "http://openweathermap.org/img/wn/02d@2x.png";
+      document.body.style.backgroundColor = "#9d9fa1";
+      setImgChange("http://openweathermap.org/img/wn/03d@2x.png");
+    } else if (desc.includes("RAIN") || desc.includes("DRIZZLE")) {
+      document.body.style.backgroundColor = "#737475";
+      setImgChange("http://openweathermap.org/img/wn/09d@2x.png");
+    } else if (desc.includes("THUNDER")) {
+      document.body.style.backgroundColor = "#4b4c4d";
+      setImgChange("http://openweathermap.org/img/wn/11d@2x.png");
+    } else if (desc.includes("SNOW")) {
+      document.body.style.backgroundColor = "#95a9bd";
+      setImgChange("http://openweathermap.org/img/wn/13d@2x.png");
     }
-  });
+  }, [desc]);
 
   return (
-    <div className="section hidden">
+    <div className={`section ${isLoading ? "hidden" : ""}`}>
       <div className="subsection">
         <span>{city}</span>
       </div>
       <div className="subsection">
-        <img className="img" alt="Current weather conditions"></img>
+        <img
+          className="img"
+          src={imgChange}
+          alt="Current weather conditions"
+        ></img>
         <span className="temp">{temp}°</span>
       </div>
       <div className="subsection">
