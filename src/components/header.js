da585e1b53adc51080095697f97a576e69225f3a
Bryant Meskill <bryant.meskill@gmail.com>
2022-05-23 11:52:09 -0500

    Removed subheader.

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,6 @@ const Header = () => {
   return (
     <div className="header">
       <h1 className="title">Simple Weather</h1>
-      <h2 className="subtitle">
-        Type in your zipcode below for simple weather information!
-      </h2>
     </div>
   );
 };
